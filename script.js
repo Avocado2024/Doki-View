2b0d237f4adb167408732369b35200360def06af
Avocado2024 <thant2023new@gmail.com>
2025-11-15 20:14:00 +0630

    Add dark mode support with ThemeManager

--- script.js
+++ script.js
@@ -65,9 +65,29 @@ class ImageViewer {
   }
 }
 
+class ThemeManager {
+  constructor() {
+    this.body = document.body;
+    this.mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    this.applyTheme(this.mediaQuery.matches); // Apply initial theme
+    this.mediaQuery.addEventListener("change", (event) => this.applyTheme(event.matches));
+  }
+
+  applyTheme(isDark) {
+    if (isDark) this.body.setAttribute("data-theme", "dark");
+    else this.body.removeAttribute("data-theme");
+  }
+}
+
+// Auto apply theme
+const themeManager = new ThemeManager();
+
+// Build reader UI
 const buildUI = new ReaderUI();
 buildUI.setTitle(data.name, data.chapter);
 buildUI.setCredits(data.team);
 
+// Init image viewer
 const imgViewer = new ImageViewer(buildUI.imgContainer, data.chapter, data.images);
 imgViewer.render();
